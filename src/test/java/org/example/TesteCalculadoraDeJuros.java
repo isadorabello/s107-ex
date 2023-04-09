394a097cb8e68989d14c95057d4c93a31420d3e3
isadorabello <isadora.bello@gec.inatel.br>
2023-04-09 15:58:48 -0300

    feat: adjusting dependencies

--- src/test/java/org/example/TesteCalculadoraDeJuros.java
+++ src/test/java/org/example/TesteCalculadoraDeJuros.java
@@ -1,7 +1,8 @@
 package org.example;
 
 import org.example.juros.*;
-import org.testng.annotations.Test;
+import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
